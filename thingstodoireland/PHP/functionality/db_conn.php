7a7c3e8f8e5acf57c39592d2e892a7ebce7f5d4a
LexisM <55384632+LexisM@users.noreply.github.com>
2024-02-12 14:14:32 +0000

    Update db_conn.php

--- thingstodoireland/PHP/functionality/db_conn.php
+++ thingstodoireland/PHP/functionality/db_conn.php
@@ -10,16 +10,6 @@ if($conn->connect_error){
 	die("FAILED connecting to database". $conn->connect_error);
 }
 
-/* $dbHost = "156.67.74.207";
-$dbUser = "u380853468_onteN";
-$dbPwd = "Whattodoireland22";
-$dbName = "u380853468_Tqxrr";
-
-$conn = mysqli_connect($dbHost,$dbUser,$dbPwd,$dbName);
-
-if($conn->connect_error){
-	die("FAILED connecting to database". $conn->connect_error);
-} */
 
 
 
